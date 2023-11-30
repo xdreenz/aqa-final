acde4e8984b3dab54b62fa0a5c16393028139d37
Akaten test <gxconv@gmail.com>
2023-11-30 18:20:30 +0300

    Using jackson-databind from now

--- src/test/java/ru/netology/aqa/data/DataHelper.java
+++ src/test/java/ru/netology/aqa/data/DataHelper.java
@@ -1,6 +1,13 @@
 package ru.netology.aqa.data;
 
 import net.datafaker.Faker;
+
+import java.io.File;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 public class DataHelper {
@@ -14,13 +21,10 @@ public class DataHelper {
 //          generateRandomCardOwnerName(), generateRandomCardCVV());
 //    }
 
-
-    public static String getCard1Number() {
-        return "4444 4444 4444 4441";
-    }
-
-    public static String getCard2Number() {
-        return "4444 4444 4444 4442";
+    public static List<CardItem> getCardItemsFromFile(String fileName) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        File jsonFile = new File(fileName);
+        return mapper.readValue(jsonFile, new TypeReference<>(){});
     }
     public static String generateRandomCardNumber() {
         return faker.finance().creditCard();
@@ -33,5 +37,19 @@ public class DataHelper {
         String cardExpireYear;
         String cardOwnerName;
         String CVC;
+    }
+
+    @Value
+    public static class CardItem {
+        String cardNumber;
+        String cardStatus;
+        public CardItem(
+                @JsonProperty("number") String cardNumber,
+                @JsonProperty("status") String cardStatus)
+        {
+            this.cardNumber = cardNumber;
+            this.cardStatus = cardStatus;
+        }
+
     }
 }
