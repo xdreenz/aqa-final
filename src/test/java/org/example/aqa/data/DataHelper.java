179b3a5cf13014c89d707b9bd67ea93eed4d00f7
Akaten test <gxconv@gmail.com>
2025-03-25 16:55:30 +0300

    Removed unused import statement.

--- src/test/java/org/example/aqa/data/DataHelper.java
+++ src/test/java/org/example/aqa/data/DataHelper.java
@@ -9,7 +9,6 @@ import java.util.List;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Value;
 import net.datafaker.providers.base.Finance;
 import org.apache.commons.lang3.StringUtils;
 
